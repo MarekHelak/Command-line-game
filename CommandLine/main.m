84df80b5152013435a1c863926bccfa746e2d361
Marek <marek.krzysztof.helak@gmail.com>
2015-04-20 19:56:24 +0200

    Initial Commit

---
+++ CommandLine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CommandLine
+//
+//  Created by Marek on 20.04.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
