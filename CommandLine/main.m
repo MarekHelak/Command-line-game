66d05580b7f5e77691350422cc7381907b385d4b
Marek <marek.krzysztof.helak@gmail.com>
2015-04-20 20:53:12 +0200

    Testing GitHub repository

--- CommandLine/main.m
+++ CommandLine/main.m
@@ -10,8 +10,35 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        //Components
+        int answer = arc4random() % 100 + 1;
+        int guess = 0;
+        int turn = 0;
+
+        //Output
+        NSLog(@"Enter a number between 1 and 100");
+        
+        //While statment
+        while (answer != guess) {
+            turn++;
+            NSLog(@"Turn: %i", turn);
+            
+            //Input
+            scanf("%i", &guess);
+        
+            //If stament
+            if (guess > answer) {
+                NSLog(@"Lower!");
+                }
+            else if (guess < answer) {
+                NSLog(@"Higher!");
+                }
+            else {
+                NSLog(@"Correct! The answer was %i", answer);
+            }
+        }
+        
     }
     return 0;
 }
